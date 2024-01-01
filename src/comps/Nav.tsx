4fc787dd8831149f52b37bbdd653550014736c5e
bilsyp <bilalsyah77@gmail.com>
2024-01-01 13:04:39 +0700

    migrate pnpm - npm

--- src/comps/Nav.tsx
+++ src/comps/Nav.tsx
@@ -7,12 +7,15 @@ import {
 import { IoSunnyOutline } from "react-icons/io5";
 import { LuMoonStar } from "react-icons/lu";
 import { useTheme } from "@/components/theme-provider";
+import { Link } from "react-router-dom";
 export default function Nav() {
   const { theme, setTheme } = useTheme();
   return (
     <nav className=" flex dark:bg-[hsl(263.4,70%,50.4%)] dark:text-white bg-gray-100  justify-between py-3 items-center px-3 lg:px-10 w-full ">
       <div className="brand">
-        <h1 className=" text-2xl  font-semibold">NDN</h1>
+        <Link to="/">
+          <h1 className=" text-2xl  font-semibold">NDN</h1>
+        </Link>
       </div>
       <div>
         <DropdownMenu>
